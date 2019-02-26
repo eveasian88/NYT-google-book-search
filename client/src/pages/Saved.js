8d4c4f2c59e3bf6cc531d0927f4ab96c5cfa0b04
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-02-25 18:17:35 -0800

    search and saved links to right places

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import SearchResults from "../components/SearchResults/SearchResults"
 
 class Saved extends React.Component {
-     
-    render(){
-        return(
-            <div>
-                saved
-            </div>
+
+    render() {
+        return (
+            <Grid>
+                <Grid.Row centered>
+                    <SearchResults />
+                </Grid.Row>
+                <Grid.Row centered>
+                    Saved Books
+                </Grid.Row>
+            </Grid>
         );
     }
 }
